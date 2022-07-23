93c50bb6774ed6a6903c6862094611767412e948
Mauro <MauroMarchesan88@gmail.com>
2022-07-22 22:00:40 -0300

    configurando CORS

--- src/api/app.js
+++ src/api/app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const rescue = require('express-rescue');
 const UserController = require('../controllers/user');
@@ -5,7 +6,13 @@ const errorMiddleware = require('../middlewares/error');
 
 const app = express();
 
-app.get('/users', rescue(UserController));
+const corsOptions = {
+    origin: 'https://git.heroku.com/herocker-exercise-46-frontend.git',
+    optionsSuccessStatus: 200,
+    // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.get('/users', cors(corsOptions), rescue(UserController));
 
 app.use(errorMiddleware); 
 
